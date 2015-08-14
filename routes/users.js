5d30a37a19f6d29eca8be97e064d1eb8c23448dd
Jacob Brenneman <j.brenneman19@gmail.com>
2015-08-14 00:40:31 -0600

    all db calls in routes refactored to use promises

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var db = require('./../lib/javascripts/mongo');
-var account = require('./../lib/javascripts/user');
 
 // SignUp / New Account
 router.get('/new_account', function (req, res, next) {
   res.render('books/new_account');
 });
 router.post('/new_account', function (req, res, next) {
-  db.createAccount(req, function (errors) {
+  db.createAccount(req).then(function (errors) {
     if(errors){
       res.render('books/new_account', {errors: errors});
     } else {
@@ -21,9 +20,9 @@ router.get('/login', function (req, res, next) {
   res.render('books/login');
 })
 router.post('/login', function (req, res, next) {
-  db.login(req, function (errors) {
+  db.login(req).then(function (errors) {
     if(errors){
-      res.render('books/login', {errors: errors})      
+      res.render('books/login', {errors: errors})
     } else {
       res.redirect('/library/books')
     }
@@ -31,9 +30,8 @@ router.post('/login', function (req, res, next) {
 })
 // Logout
 router.get('/logout', function (req, res, next) {
-  account.logout(req, function () {
-    res.redirect('/');
-  })
+  req.session = null;
+  res.redirect('/');
 });
 
 module.exports = router;

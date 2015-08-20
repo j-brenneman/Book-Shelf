a645fd807fd053c7d4c47bc291441397185dace1
Jacob Brenneman <j.brenneman19@gmail.com>
2015-08-20 15:06:28 -0600

    fixed find book to display quotes on edit page

---
+++ public/javascripts/quotes.js
@@ -0,0 +1,13 @@
+var section = document.getElementsByClassName('collection');
+var show = document.getElementsByTagName('h4');
+console.log(section);
+console.log(show);
+
+for (var i = 0; i < show.length; i++) {
+  show[i].quotes = section[i].childNodes[2]
+  show[i].childNodes[1].addEventListener('click', display.bind(show[i]))
+}
+
+function display () {
+  $(this.quotes).toggle();
+}
--- routes/books/index.js
+++ routes/books/index.js
@@ -5,7 +5,6 @@ var db = require('./../../lib/javascripts/mongo.js');
 // My Shelf
 router.get('/books/shelf/:id', function (req, res, next) {
   db.findBooks(req).then(function (collection) {
-    console.log(collection[0].quotes[0].quote);
     res.render('books/shelf', {collection: collection})
   })
 })
@@ -18,11 +17,10 @@ router.post('/books/:id', function (req, res, next) {
 // Edit Book
 router.get('/books/:id', function (req, res, next) {
   db.findBook(req).then(function (book) {
-    console.log(book);
-    res.render('books/edit', {book: book[0]})
+    console.log(book.quotes);
+    res.render('books/edit', {book: book})
   })
 })
-
 router.post('/books/:id/update', function (req, res, next) {
   db.updateBook(req).then(function () {
     res.redirect('/library/books/shelf/' + res.locals.userId)
